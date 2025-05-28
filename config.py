1b1a75e442c76387bb21444b8f9577a6953871d1
StevenAC369 <84295861+StevenAC369@users.noreply.github.com>
2025-05-28 10:08:17 -0500

    config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ DB_PORT = "5432"  # Puerto por defecto de PostgreSQL
 
 #----------------------------------------- Configuracion de Paypal ---------------------------------------------------------
 # Correo de PayPal
-CORREO_PAYPAL = "chiripa013@gmail.com"
+CORREO_PAYPAL = "..."
 # Descripcion de PayPal
 ITEM_NAME = 'Tienda de Electronica by Steven' 
